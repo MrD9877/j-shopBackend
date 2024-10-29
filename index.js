16f139a7a329e7c1506915ad766fd21fb8d98bcd
Dhuruv Bansal <Dhuruvbansal53@gmail.com>
2024-10-29 09:50:23 +0530

    9

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ app.use(session({
         maxAge: 60000 * 60,
         httpOnly: false,
         withCredentials: true,
-        secure: isSecure,      // Send cookie only over HTTPS
         sameSite: 'Lax',
     },
     store: MongoStore.create({
