5041572c035b274862a49fd6b384dcf2fcf5ae65
Dhuruv Bansal <Dhuruvbansal53@gmail.com>
2024-12-17 16:17:01 +0530

    try proxy

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ export const useApp = () => {
   );
   app.use(
     session({
+      proxy: true,
       secret: process.env.SESSION_SECRET, // sign session with this
       resave: false, //store on every request when true usefull if store have expiration date
       saveUninitialized: false, //if true session will be saved even if no modification
